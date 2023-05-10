c2c2f83bc1e656b45646db3b7e1535c8777ebbb2
UserIryna28 <Ltdcom@ukr.net>
2023-05-10 10:16:39 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,10 @@
 //const dataStart = document.querySelector("[data-start]"),
 //const dataStop = document.querySelector("[data-stop]"),
-//dataStart.addEvenListener("click", () => {})
+//const timerId = null
+//dataStart.addEvenListener("click", () => {
+//timerId = setInterval
+//})
 
 
 //dataStopaddEvenListener("click", () => {})
+console.log("Hello")

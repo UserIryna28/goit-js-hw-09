e4483e579f695ac402841570d6aa07962f5ec077
UserIryna28 <Ltdcom@ukr.net>
2023-05-10 10:08:53 +0300

    Adds changes

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,6 @@
+//const dataStart = document.querySelector("[data-start]"),
+//const dataStop = document.querySelector("[data-stop]"),
+//dataStart.addEvenListener("click", () => {})
 
+
+//dataStopaddEvenListener("click", () => {})

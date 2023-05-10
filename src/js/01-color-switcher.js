a71197af5155fd9da4e464d6ed5e4cc6b3b34890
UserIryna28 <Ltdcom@ukr.net>
2023-05-10 10:43:04 +0300

    Add files h-w 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,10 +1,28 @@
-//const dataStart = document.querySelector("[data-start]"),
-//const dataStop = document.querySelector("[data-stop]"),
-//const timerId = null
-//dataStart.addEvenListener("click", () => {
-//timerId = setInterval
-//})
+const dataStart = document.querySelector("[data-start]");
+const dataStop = document.querySelector("[data-stop]");
+const body = document.querySelector('body');
+
+const timerId = null
+dataStart.addEvenListener("click", onStartClick) 
+dataStop.addEvenListener("click", onStopClick)
+
+function onStartClick(){
+    dataStart.disabled = true
+timerId = setInterval(changeBgColor, 1000)
+}
+
+function onStopClick() {
+    dataStart.disabled = false
+    clearInterval(timerId)
+}
+
+function changeBgColor() {
+    const color = getRandomHexColor
+    body.style.backgroundColor = color
+}
 
 
-//dataStopaddEvenListener("click", () => {})
-console.log("Hello")
+
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}

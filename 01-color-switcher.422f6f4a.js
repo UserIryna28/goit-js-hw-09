829f4fe10c9fc469d6d07acc38a625f977539a5c
UserIryna28 <121198479+UserIryna28@users.noreply.github.com>
2023-05-10 08:12:46 +0000

    Deploying to gh-pages from @ UserIryna28/goit-js-hw-09@e7f3f33b3322c07db56887eec99a49a21d4a8977 🚀

---
+++ 01-color-switcher.422f6f4a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.body;let o=null;function a(){const t=r;n.style.backgroundColor=t}function r(){return`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(function(){t.disabled=!0,o=setInterval(a,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.422f6f4a.js.map

08b8cbfb53e22934431671bc2d4b964cef255ad3
UserIryna28 <Ltdcom@ukr.net>
2023-05-11 15:25:48 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,31 +1,30 @@
-const dataStart = document.querySelector("[data-start]");
-const dataStop = document.querySelector("[data-stop]");
+
+const dataStart = document.querySelector('[data-start]');
+const dataStop = document.querySelector('[data-stop]');
+
 const body = document.body;
+let intervalId = null;
 
-let timerId = null
-
+dataStart.addEventListener('click', onStartClick);
+dataStop.addEventListener('click', onStopClick);
 
 function onStartClick() {
-    //якщо кнопка onStartClick натиснена, disabled забороняє повторне натискання
-    dataStart.disabled = true
-    //при натисненні onStartClick відбувається changeBgColor з інтервалом 1с.
-timerId = setInterval(changeBgColor, 1000)
+     //якщо кнопка onStartClick натиснена, disabled забороняє повторне натискання
+    dataStart.disabled = true; 
+    // // при натисненні onStartClick відбувається changeBgColor з інтервалом 1с.
+    intervalId = setInterval(changeBgColor, 1000); 
 }
-
+// якщо кнопка onStopClick натиснена - зупиняємо зміну backgroundColor
 function onStopClick() {
-    dataStart.disabled = false
-    //якщо кнопка onStopClick натиснена - зупиняємо зміну backgroundColor
-    clearInterval(timerId)
+    dataStart.disabled = false; 
+    clearInterval(intervalId); 
 }
-
+// //для генерування випадкового кольору використовуємо функцію getRandomHexColor
 function changeBgColor() {
-    // для генерування випадкового кольору використовуємо функцію getRandomHexColor
-    const color = getRandomHexColor()
-    body.style.backgroundColor = color
+    const color = getRandomHexColor();
+    body.style.backgroundColor = color;
 }
-
-
 
 function getRandomHexColor() {
-   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }

f268d183e5d5b4fdbbb1bd076fcb3b2a344f51ac
UserIryna28 <Ltdcom@ukr.net>
2023-05-12 14:34:34 +0300

    Add changes to h-w 9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from "notiflix";
+
+const form = document.querySelector(".form")
+
+
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+const delay = Number(form.delay.value)
+const step = Number(form.step.value)
+const amount = Number(form.amount.value)
+
+  for (let i = 1; i <= amount; i+=1) {
+    createPromise(i, delay + (i - 1) * step)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    
+    
   }
+})
+
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay)
+  })
 }
